5738199b53212c44fae5a977d9577fa41c98988b
Russ36363 <russ36363@gmail.com>
2012-12-06 15:04:24 -0500

    Added working enc and dec test scripts

---
+++ testdec.sh
@@ -0,0 +1,300 @@
+echo i=1
+cpabe-dec -k -o file1.dec priv1 file1
+cpabe-dec -k -o file1n.dec priv1n file1n
+echo i=2
+cpabe-dec -k -o file2.dec priv2 file2
+cpabe-dec -k -o file2n.dec priv2n file2n
+echo i=3
+cpabe-dec -k -o file3.dec priv3 file3
+cpabe-dec -k -o file3n.dec priv3n file3n
+echo i=4
+cpabe-dec -k -o file4.dec priv4 file4
+cpabe-dec -k -o file4n.dec priv4n file4n
+echo i=5
+cpabe-dec -k -o file5.dec priv5 file5
+cpabe-dec -k -o file5n.dec priv5n file5n
+echo i=6
+cpabe-dec -k -o file6.dec priv6 file6
+cpabe-dec -k -o file6n.dec priv6n file6n
+echo i=7
+cpabe-dec -k -o file7.dec priv7 file7
+cpabe-dec -k -o file7n.dec priv7n file7n
+echo i=8
+cpabe-dec -k -o file8.dec priv8 file8
+cpabe-dec -k -o file8n.dec priv8n file8n
+echo i=9
+cpabe-dec -k -o file9.dec priv9 file9
+cpabe-dec -k -o file9n.dec priv9n file9n
+echo i=10
+cpabe-dec -k -o file10.dec priv10 file10
+cpabe-dec -k -o file10n.dec priv10n file10n
+echo i=11
+cpabe-dec -k -o file11.dec priv11 file11
+cpabe-dec -k -o file11n.dec priv11n file11n
+echo i=12
+cpabe-dec -k -o file12.dec priv12 file12
+cpabe-dec -k -o file12n.dec priv12n file12n
+echo i=13
+cpabe-dec -k -o file13.dec priv13 file13
+cpabe-dec -k -o file13n.dec priv13n file13n
+echo i=14
+cpabe-dec -k -o file14.dec priv14 file14
+cpabe-dec -k -o file14n.dec priv14n file14n
+echo i=15
+cpabe-dec -k -o file15.dec priv15 file15
+cpabe-dec -k -o file15n.dec priv15n file15n
+echo i=16
+cpabe-dec -k -o file16.dec priv16 file16
+cpabe-dec -k -o file16n.dec priv16n file16n
+echo i=17
+cpabe-dec -k -o file17.dec priv17 file17
+cpabe-dec -k -o file17n.dec priv17n file17n
+echo i=18
+cpabe-dec -k -o file18.dec priv18 file18
+cpabe-dec -k -o file18n.dec priv18n file18n
+echo i=19
+cpabe-dec -k -o file19.dec priv19 file19
+cpabe-dec -k -o file19n.dec priv19n file19n
+echo i=20
+cpabe-dec -k -o file20.dec priv20 file20
+cpabe-dec -k -o file20n.dec priv20n file20n
+echo i=21
+cpabe-dec -k -o file21.dec priv21 file21
+cpabe-dec -k -o file21n.dec priv21n file21n
+echo i=22
+cpabe-dec -k -o file22.dec priv22 file22
+cpabe-dec -k -o file22n.dec priv22n file22n
+echo i=23
+cpabe-dec -k -o file23.dec priv23 file23
+cpabe-dec -k -o file23n.dec priv23n file23n
+echo i=24
+cpabe-dec -k -o file24.dec priv24 file24
+cpabe-dec -k -o file24n.dec priv24n file24n
+echo i=25
+cpabe-dec -k -o file25.dec priv25 file25
+cpabe-dec -k -o file25n.dec priv25n file25n
+echo i=26
+cpabe-dec -k -o file26.dec priv26 file26
+cpabe-dec -k -o file26n.dec priv26n file26n
+echo i=27
+cpabe-dec -k -o file27.dec priv27 file27
+cpabe-dec -k -o file27n.dec priv27n file27n
+echo i=28
+cpabe-dec -k -o file28.dec priv28 file28
+cpabe-dec -k -o file28n.dec priv28n file28n
+echo i=29
+cpabe-dec -k -o file29.dec priv29 file29
+cpabe-dec -k -o file29n.dec priv29n file29n
+echo i=30
+cpabe-dec -k -o file30.dec priv30 file30
+cpabe-dec -k -o file30n.dec priv30n file30n
+echo i=31
+cpabe-dec -k -o file31.dec priv31 file31
+cpabe-dec -k -o file31n.dec priv31n file31n
+echo i=32
+cpabe-dec -k -o file32.dec priv32 file32
+cpabe-dec -k -o file32n.dec priv32n file32n
+echo i=33
+cpabe-dec -k -o file33.dec priv33 file33
+cpabe-dec -k -o file33n.dec priv33n file33n
+echo i=34
+cpabe-dec -k -o file34.dec priv34 file34
+cpabe-dec -k -o file34n.dec priv34n file34n
+echo i=35
+cpabe-dec -k -o file35.dec priv35 file35
+cpabe-dec -k -o file35n.dec priv35n file35n
+echo i=36
+cpabe-dec -k -o file36.dec priv36 file36
+cpabe-dec -k -o file36n.dec priv36n file36n
+echo i=37
+cpabe-dec -k -o file37.dec priv37 file37
+cpabe-dec -k -o file37n.dec priv37n file37n
+echo i=38
+cpabe-dec -k -o file38.dec priv38 file38
+cpabe-dec -k -o file38n.dec priv38n file38n
+echo i=39
+cpabe-dec -k -o file39.dec priv39 file39
+cpabe-dec -k -o file39n.dec priv39n file39n
+echo i=40
+cpabe-dec -k -o file40.dec priv40 file40
+cpabe-dec -k -o file40n.dec priv40n file40n
+echo i=41
+cpabe-dec -k -o file41.dec priv41 file41
+cpabe-dec -k -o file41n.dec priv41n file41n
+echo i=42
+cpabe-dec -k -o file42.dec priv42 file42
+cpabe-dec -k -o file42n.dec priv42n file42n
+echo i=43
+cpabe-dec -k -o file43.dec priv43 file43
+cpabe-dec -k -o file43n.dec priv43n file43n
+echo i=44
+cpabe-dec -k -o file44.dec priv44 file44
+cpabe-dec -k -o file44n.dec priv44n file44n
+echo i=45
+cpabe-dec -k -o file45.dec priv45 file45
+cpabe-dec -k -o file45n.dec priv45n file45n
+echo i=46
+cpabe-dec -k -o file46.dec priv46 file46
+cpabe-dec -k -o file46n.dec priv46n file46n
+echo i=47
+cpabe-dec -k -o file47.dec priv47 file47
+cpabe-dec -k -o file47n.dec priv47n file47n
+echo i=48
+cpabe-dec -k -o file48.dec priv48 file48
+cpabe-dec -k -o file48n.dec priv48n file48n
+echo i=49
+cpabe-dec -k -o file49.dec priv49 file49
+cpabe-dec -k -o file49n.dec priv49n file49n
+echo i=50
+cpabe-dec -k -o file50.dec priv50 file50
+cpabe-dec -k -o file50n.dec priv50n file50n
+echo i=51
+cpabe-dec -k -o file51.dec priv51 file51
+cpabe-dec -k -o file51n.dec priv51n file51n
+echo i=52
+cpabe-dec -k -o file52.dec priv52 file52
+cpabe-dec -k -o file52n.dec priv52n file52n
+echo i=53
+cpabe-dec -k -o file53.dec priv53 file53
+cpabe-dec -k -o file53n.dec priv53n file53n
+echo i=54
+cpabe-dec -k -o file54.dec priv54 file54
+cpabe-dec -k -o file54n.dec priv54n file54n
+echo i=55
+cpabe-dec -k -o file55.dec priv55 file55
+cpabe-dec -k -o file55n.dec priv55n file55n
+echo i=56
+cpabe-dec -k -o file56.dec priv56 file56
+cpabe-dec -k -o file56n.dec priv56n file56n
+echo i=57
+cpabe-dec -k -o file57.dec priv57 file57
+cpabe-dec -k -o file57n.dec priv57n file57n
+echo i=58
+cpabe-dec -k -o file58.dec priv58 file58
+cpabe-dec -k -o file58n.dec priv58n file58n
+echo i=59
+cpabe-dec -k -o file59.dec priv59 file59
+cpabe-dec -k -o file59n.dec priv59n file59n
+echo i=60
+cpabe-dec -k -o file60.dec priv60 file60
+cpabe-dec -k -o file60n.dec priv60n file60n
+echo i=61
+cpabe-dec -k -o file61.dec priv61 file61
+cpabe-dec -k -o file61n.dec priv61n file61n
+echo i=62
+cpabe-dec -k -o file62.dec priv62 file62
+cpabe-dec -k -o file62n.dec priv62n file62n
+echo i=63
+cpabe-dec -k -o file63.dec priv63 file63
+cpabe-dec -k -o file63n.dec priv63n file63n
+echo i=64
+cpabe-dec -k -o file64.dec priv64 file64
+cpabe-dec -k -o file64n.dec priv64n file64n
+echo i=65
+cpabe-dec -k -o file65.dec priv65 file65
+cpabe-dec -k -o file65n.dec priv65n file65n
+echo i=66
+cpabe-dec -k -o file66.dec priv66 file66
+cpabe-dec -k -o file66n.dec priv66n file66n
+echo i=67
+cpabe-dec -k -o file67.dec priv67 file67
+cpabe-dec -k -o file67n.dec priv67n file67n
+echo i=68
+cpabe-dec -k -o file68.dec priv68 file68
+cpabe-dec -k -o file68n.dec priv68n file68n
+echo i=69
+cpabe-dec -k -o file69.dec priv69 file69
+cpabe-dec -k -o file69n.dec priv69n file69n
+echo i=70
+cpabe-dec -k -o file70.dec priv70 file70
+cpabe-dec -k -o file70n.dec priv70n file70n
+echo i=71
+cpabe-dec -k -o file71.dec priv71 file71
+cpabe-dec -k -o file71n.dec priv71n file71n
+echo i=72
+cpabe-dec -k -o file72.dec priv72 file72
+cpabe-dec -k -o file72n.dec priv72n file72n
+echo i=73
+cpabe-dec -k -o file73.dec priv73 file73
+cpabe-dec -k -o file73n.dec priv73n file73n
+echo i=74
+cpabe-dec -k -o file74.dec priv74 file74
+cpabe-dec -k -o file74n.dec priv74n file74n
+echo i=75
+cpabe-dec -k -o file75.dec priv75 file75
+cpabe-dec -k -o file75n.dec priv75n file75n
+echo i=76
+cpabe-dec -k -o file76.dec priv76 file76
+cpabe-dec -k -o file76n.dec priv76n file76n
+echo i=77
+cpabe-dec -k -o file77.dec priv77 file77
+cpabe-dec -k -o file77n.dec priv77n file77n
+echo i=78
+cpabe-dec -k -o file78.dec priv78 file78
+cpabe-dec -k -o file78n.dec priv78n file78n
+echo i=79
+cpabe-dec -k -o file79.dec priv79 file79
+cpabe-dec -k -o file79n.dec priv79n file79n
+echo i=80
+cpabe-dec -k -o file80.dec priv80 file80
+cpabe-dec -k -o file80n.dec priv80n file80n
+echo i=81
+cpabe-dec -k -o file81.dec priv81 file81
+cpabe-dec -k -o file81n.dec priv81n file81n
+echo i=82
+cpabe-dec -k -o file82.dec priv82 file82
+cpabe-dec -k -o file82n.dec priv82n file82n
+echo i=83
+cpabe-dec -k -o file83.dec priv83 file83
+cpabe-dec -k -o file83n.dec priv83n file83n
+echo i=84
+cpabe-dec -k -o file84.dec priv84 file84
+cpabe-dec -k -o file84n.dec priv84n file84n
+echo i=85
+cpabe-dec -k -o file85.dec priv85 file85
+cpabe-dec -k -o file85n.dec priv85n file85n
+echo i=86
+cpabe-dec -k -o file86.dec priv86 file86
+cpabe-dec -k -o file86n.dec priv86n file86n
+echo i=87
+cpabe-dec -k -o file87.dec priv87 file87
+cpabe-dec -k -o file87n.dec priv87n file87n
+echo i=88
+cpabe-dec -k -o file88.dec priv88 file88
+cpabe-dec -k -o file88n.dec priv88n file88n
+echo i=89
+cpabe-dec -k -o file89.dec priv89 file89
+cpabe-dec -k -o file89n.dec priv89n file89n
+echo i=90
+cpabe-dec -k -o file90.dec priv90 file90
+cpabe-dec -k -o file90n.dec priv90n file90n
+echo i=91
+cpabe-dec -k -o file91.dec priv91 file91
+cpabe-dec -k -o file91n.dec priv91n file91n
+echo i=92
+cpabe-dec -k -o file92.dec priv92 file92
+cpabe-dec -k -o file92n.dec priv92n file92n
+echo i=93
+cpabe-dec -k -o file93.dec priv93 file93
+cpabe-dec -k -o file93n.dec priv93n file93n
+echo i=94
+cpabe-dec -k -o file94.dec priv94 file94
+cpabe-dec -k -o file94n.dec priv94n file94n
+echo i=95
+cpabe-dec -k -o file95.dec priv95 file95
+cpabe-dec -k -o file95n.dec priv95n file95n
+echo i=96
+cpabe-dec -k -o file96.dec priv96 file96
+cpabe-dec -k -o file96n.dec priv96n file96n
+echo i=97
+cpabe-dec -k -o file97.dec priv97 file97
+cpabe-dec -k -o file97n.dec priv97n file97n
+echo i=98
+cpabe-dec -k -o file98.dec priv98 file98
+cpabe-dec -k -o file98n.dec priv98n file98n
+echo i=99
+cpabe-dec -k -o file99.dec priv99 file99
+cpabe-dec -k -o file99n.dec priv99n file99n
+echo i=100
+cpabe-dec -k -o file100.dec priv100 file100
+cpabe-dec -k -o file100n.dec priv100n file100n
