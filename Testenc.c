192be6ea622f168c7695e583584810ba890fbee3
Russ <russ36363@gmail.com>
2012-12-06 14:31:17 -0500

    Update Testenc.c
    Initial Full script

--- Testenc.c
+++ Testenc.c
@@ -20,28 +20,28 @@ int main(int argc, char** argv)
   {
     //Horizontal
     fprintf(file,"%s%d%s","echo i=",i,"\n");
-    fprintf(file,"%s%d %s","cpabe-keygen -o priv",i,"pub_key master_key ");
+    fprintf(file,"%s%d %s","cpabe-enc -o file",i,"pub_key testout ");
     j=1;
     while(j<=i)
     {
       fprintf(file,"%s%d","prop",j);
       if(j<i)
       {
-        fprintf(file,"%s"," ");
+        fprintf(file,"%s"," and ");
       }
       j++;
     }
     fprintf(file,"%s","\n");
     
     //Vertical
-    fprintf(file,"%s%d%s","cpabe-keygen -o priv",i,"n pub_key master_key ");
+    fprintf(file,"%s%d%s","cpabe-enc -o file",i,"n pub_key testout ");
     if(i<64)
     {
-      fprintf(file,"%s%d%s","\"prop = 1#",i,"\"\n");
+      fprintf(file,"%s%d%s","\"prop <=",((unsigned long long)1<<(i-1)),"\"\n");
     }
     else
     {
-      fprintf(file,"%s%d%s","\"prop = 1#63\" \"prop2 = 1#",i-63,"\"\n");
+      fprintf(file,"%s%d%s","\"prop <=",((unsigned long long)1<<63),"\" \"prop2 <=",((unsigned long long)1<<(i-64)),"\"\n");
     }
   }
   fclose(file);
