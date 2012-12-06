9d2b4aa4aedfc4a0b28a8349ce7e1569137dde8c
Russ <russ36363@gmail.com>
2012-12-06 14:39:44 -0500

    Update Testenc.c
    Fixed printf statements

--- Testenc.c
+++ Testenc.c
@@ -37,11 +37,11 @@ int main(int argc, char** argv)
     fprintf(file,"%s%d%s","cpabe-enc -o file",i,"n pub_key testout ");
     if(i<64)
     {
-      fprintf(file,"%s%d%s","\"prop <=",((unsigned long long)1<<(i-1)),"\"\n");
+      fprintf(file,"%s%llu%s","\"prop <=",((unsigned long long)1<<(i-1)),"\"\n");
     }
     else
     {
-      fprintf(file,"%s%d%s","\"prop <=",((unsigned long long)1<<63),"\" \"prop2 <=",((unsigned long long)1<<(i-64)),"\"\n");
+      fprintf(file,"%s%llu%s%llu%s","\"prop <=",((unsigned long long)1<<63),"\" \"prop2 <=",((unsigned long long)1<<(i-64)),"\"\n");
     }
   }
   fclose(file);
