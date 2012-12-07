215a8bce0a5c8cc37937228a3ec95ec32ab83d55
Russ <russ36363@gmail.com>
2012-12-07 14:41:59 -0500

    Update Testenc.c
    Test commit, revert if doesn't work.

--- Testenc.c
+++ Testenc.c
@@ -35,14 +35,14 @@ int main(int argc, char** argv)
     //Send Control-D to end input
     
     //Vertical
-    fprintf(file,"%s%d%s","cpabe-enc -o file",i,"n -k pub_key testout '");
+    fprintf(file,"%s%d%s","cpabe-enc -o file",i,"n -k pub_key testout \"");
     if(i<64)
     {
-      fprintf(file,"%s%llu%s","\"prop <= ",((unsigned long long)1<<(i-1)),"\"\n");
+      fprintf(file,"%s%llu%s","'prop <= ",((unsigned long long)1<<(i-1)),"'\n");
     }
     else
     {
-      fprintf(file,"%s%llu%s%llu%s","\"prop <= ",((unsigned long long)1<<63),"\" and \"prop2 <= ",((unsigned long long)1<<(i-64)),"\"'\n");
+      fprintf(file,"%s%llu%s%llu%s","'prop <= ",((unsigned long long)1<<63),"' and 'prop2 <= ",((unsigned long long)1<<(i-64)),"'\"\n");
     }
   }
   fclose(file);
